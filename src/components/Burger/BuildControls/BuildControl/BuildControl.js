7bb18a3094f11a76ef162c9d377d5c292c202802
Vineet Monga <vinmong1@in.ibm.com>
2020-10-12 12:08:32 +0530

    Implemented Add and Remove button and also the price calculation for the customised burger

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,8 +6,8 @@ const buildcontrol = (props) => (
     <div className={classes.Label}>
       {props.label}
     </div>
-    <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button disabled={props.disabled} className={classes.Less} onClick={props.removed}>Less</button>
+    <button className={classes.More} onClick={props.added}>More</button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,8 +10,16 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label}/>
+      <BuildControl
+        disabled={props.disabled[ctrl.type]}
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+
+      />
     ))}
   </div>
 )
